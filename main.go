c0c51f8c3ee29026421a445daed1d699490c96a7
Dhiraj Mali <95579611+dhirajmali17@users.noreply.github.com>
2025-11-18 19:31:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello! This is a simple Go web server.\n")
 		fmt.Fprintf(w, "This is a change version of this file.\n")
+		fmt.Fprintf(w, "Satyajit Bhai jindabad.\n")
 	})
 
 	fmt.Println("Server running on http://localhost:8080")
